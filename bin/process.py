c787c2174fa5aacc597585925b068046a1efc7a0
zhangjie18 <zhangjie18@kuaishou.com>
2023-03-26 22:45:33 +0800

    feat(file): add file read

--- bin/process.py
+++ bin/process.py
@@ -6,16 +6,32 @@ Date: 2023/03/26 21:00
 """
 import os
 import sys
+import logging
+import pandas as pd
 
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, '%s/../' % CUR_DIR)
 sys.path.insert(0, '%s/../lib/' % CUR_DIR)
+
+import log
+log.init_log("%s/../log/process" % CUR_DIR, logger_name='process')
+logger = logging.getLogger("process")
 
 def main():
     """
     main
     """
     print("begin to deal!")
+
+    demo_file = "%s/../data/need_process_dir/demo.xlsx" % CUR_DIR
+    logger.info(demo_file)
+
+    # pd.set_option('display.notebook_repr_html', False)
+    content = pd.read_excel(io=demo_file, sheet_name=0)
+    print(content)
+
+
+
 
 if __name__ == "__main__":
     main()
---
+++ lib/log.py
@@ -0,0 +1,68 @@
+"""
+init log
+
+File: log.py
+Author: Anonymous
+Date: 2015/09/08 16:57:24
+"""
+
+import os
+import logging.handlers
+
+def init_log(log_path, level=logging.INFO, when="D", backup=7,
+             format="%(levelname)s: %(asctime)s: %(filename)s:%(lineno)d * %(thread)d %(message)s",
+             datefmt="%m-%d %H:%M:%S", logger_name=''):
+    """
+    init_log - initialize log module
+
+    Args:
+      log_path      - Log file path prefix.
+                      Log data will go to two files: log_path.log and log_path.log.wf
+                      Any non-exist parent directories will be created automatically
+      level         - msg above the level will be displayed
+                      DEBUG < INFO < WARNING < ERROR < CRITICAL
+                      the default value is logging.INFO
+      when          - how to split the log file by time interval
+                      'S' : Seconds
+                      'M' : Minutes
+                      'H' : Hours
+                      'D' : Days
+                      'W' : Week day
+                      default value: 'D'
+      format        - format of the log
+                      default format:
+                      %(levelname)s: %(asctime)s: %(filename)s:%(lineno)d * %(thread)d %(message)s
+                      INFO: 12-09 18:02:42: log.py:40 * 139814749787872 HELLO WORLD
+      backup        - how many backup file to keep
+                      default value: 7
+
+    Raises:
+        OSError: fail to create log directories
+        IOError: fail to open log file
+    """
+    formatter = logging.Formatter(format, datefmt)
+    if logger_name != '':
+        logger = logging.getLogger(logger_name)
+    else:
+        logger = logging.getLogger(log_path.split("/").pop())
+
+    logger.setLevel(level)
+
+    dir = os.path.dirname(log_path)
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+    handler = logging.handlers.TimedRotatingFileHandler(log_path + ".log",
+                                                        when=when,
+                                                        backupCount=backup)
+    handler.setLevel(level)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    handler = logging.handlers.TimedRotatingFileHandler(log_path + ".log.wf",
+                                                        when=when,
+                                                        backupCount=backup)
+    handler.setLevel(logging.WARNING)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
